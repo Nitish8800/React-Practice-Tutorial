768f239f5bce3c449c06ba18c3180a915f19b0ec
Nitish8800 <nitishfea120784@gmail.com>
2022-06-07 00:02:17 +0530

    click event

--- react tutorial #2/react_2/src/App.js
+++ react tutorial #2/react_2/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 function App() {
   let data = "Nitish Kumar";
   function apple() {
-    data="peter";
+    data = "peter";
     alert(data);
   }
   return (
     <div className="App">
+
       <h1>{data}</h1>
       <button onClick={apple}>Click Me</button>
+      
     </div>
   );
 }

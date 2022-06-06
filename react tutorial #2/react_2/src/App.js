b7b564543395333519202b29ddb7e010de8e7ecf
Nitish8800 <nitishfea120784@gmail.com>
2022-06-07 00:07:23 +0530

    state-fun

--- react tutorial #2/react_2/src/App.js
+++ react tutorial #2/react_2/src/App.js
@@ -1,16 +1,17 @@
+import { useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
+
 function App() {
-  let data = "Nitish Kumar";
-  function apple() {
-    data = "peter";
-    alert(data);
+  const [data, setData] = useState(0);
+  function updateData() {
+    setData(data + 1);
   }
+  console.warn("______");
   return (
     <div className="App">
-
       <h1>{data}</h1>
-      <button onClick={apple}>Click Me</button>
-      
+      <button onClick={updateData}>Update Data</button>
     </div>
   );
 }

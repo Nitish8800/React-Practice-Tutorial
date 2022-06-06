1564dda8bd5240b4cafefc36b6a70415dbf96085
Nitish8800 <nitishfea120784@gmail.com>
2022-06-06 23:50:31 +0530

    commit

--- react tutorial #2/react_2/src/App.js
+++ react tutorial #2/react_2/src/App.js
@@ -1,15 +1,14 @@
-import './App.css';
+import "./App.css";
 function App() {
-  let data="Nitish Kumar";
-  function apple()
-  {
-  // data="peter";
+  let data = "Nitish Kumar";
+  function apple() {
+    data="peter";
     alert(data);
   }
   return (
     <div className="App">
-     <h1>{data}</h1>
-     <button onClick={apple}>Click Me</button>
+      <h1>{data}</h1>
+      <button onClick={apple}>Click Me</button>
     </div>
   );
 }

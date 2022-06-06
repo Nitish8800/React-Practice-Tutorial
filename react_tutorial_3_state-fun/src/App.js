dc4924d3dea484729913118430378d49f4c3127b
Nitish8800 <nitishfea120784@gmail.com>
2022-06-07 00:45:19 +0530

    props with functional C

--- react_tutorial_3_state-fun/src/App.js
+++ react_tutorial_3_state-fun/src/App.js
@@ -22,27 +22,55 @@
 
 // <------------------------------>/                       State with Class components         </===============->/
 
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       data: 0,
+//     };
+//   }
+//   apple() {
+//     this.setState({ data: this.state.data + 1 });
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>{this.state.data}</h1>
+//         <button onClick={() => this.apple()}>Update Data</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+// <------------------------------>/                       Props with Functional Component      </===============->/
+
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: 1,
-    };
+import Student from "./components/Student";
+function App() {
+  const [name, setName] = useState("Nitish Kumar");
+  function apple() {
+    alert("hello");
   }
-  apple() {
-    this.setState({ data: this.state.data + 1 });
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>{this.state.data}</h1>
-        <button onClick={() => this.apple()}>Update Data</button>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Props in React :)</h1>
+      <Student name={name} />
+      <button
+        onClick={() => {
+          setName("Niku");
+        }}
+      >
+        Update Name
+      </button>
+    </div>
+  );
 }
 
 export default App;

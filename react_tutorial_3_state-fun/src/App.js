9791b9aad259582967e13570abdf4d5e7bdc97a5
Nitish8800 <nitishfea120784@gmail.com>
2022-06-07 00:34:26 +0530

    state-class function

--- react_tutorial_3_state-fun/src/App.js
+++ react_tutorial_3_state-fun/src/App.js
@@ -1,19 +1,48 @@
-import { useState } from "react";
+// <------------------------------>/                       / // State with Functional components       </===============->/
+
+// import { useState } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   const [data, setData] = useState(0);
+//   function updateData() {
+//     setData(data + 1);
+//   }
+//   console.warn("______");
+//   return (
+//     <div className="App">
+//       <h1>{data}</h1>
+//       <button onClick={updateData}>Update Data</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// <------------------------------>/                       State with Class components         </===============->/
+
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-function App() {
-  const [data, setData] = useState(0);
-  function updateData() {
-    setData(data + 1);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: 1,
+    };
   }
-  console.warn("______");
-  return (
-    <div className="App">
-      <h1>{data}</h1>
-      <button onClick={updateData}>Update Data</button>
-    </div>
-  );
+  apple() {
+    this.setState({ data: this.state.data + 1 });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.data}</h1>
+        <button onClick={() => this.apple()}>Update Data</button>
+      </div>
+    );
+  }
 }
 
 export default App;

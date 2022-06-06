9f1ec8d84d691d34936810c4b35ffeb563c4c3e1
Nitish8800 <nitishfea120784@gmail.com>
2022-06-06 23:45:49 +0530

    commit

--- react tutorial #2/react_2/src/App.js
+++ react tutorial #2/react_2/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  let data="anil sidhu";
+  function apple()
+  {
+  data="peter";
+    alert(data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{data}</h1>
+     <button onClick={apple}>Click Me</button>
     </div>
   );
 }

7cd2e84b2ba4255475daef8228dda93e9daee623
Nitish8800 <nitishfea120784@gmail.com>
2022-06-10 02:37:10 +0530

    React tutorial in Hindi #35 Nested List in ReactJs

--- react_tutorial_3_state-fun/src/App.js
+++ react_tutorial_3_state-fun/src/App.js
@@ -523,6 +523,48 @@
 // // <------------------------------>/                 React tutorial in Hindi #34 List with Bootstrap Table
 // </===============->/
 
+// import "./App.css";
+// import React from "react";
+// import { Table } from "react-bootstrap";
+// function App() {
+//   const users = [
+//     { name: "Anil", email: "anil@test.com", contact: "111" },
+//     { name: "Burce", email: "bruce@test.com", contact: "222" },
+//     { name: "Peter", email: "peter@test.com", contact: "111" },
+//     { name: "Sam", email: "sam@test.com", contact: "777" },
+//   ];
+//   return (
+//     <div className="App">
+//       <h1>List With Bootstrap Table</h1>
+//       <Table striped variant="dark">
+//         <tbody>
+//           <tr>
+//             <td>S.No</td>
+//             <td>Name</td>
+//             <td>Email</td>
+//             <td>Contacts</td>
+//           </tr>
+//           {users.map((item, i) =>
+//             item.contact === "111" ? (
+//               <tr key={i}>
+//                 <td>{i}</td>
+//                 <td>{item.name}</td>
+//                 <td>{item.email}</td>
+//                 <td>{item.contact}</td>
+//               </tr>
+//             ) : null
+//           )}
+//         </tbody>
+//       </Table>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// // <------------------------------>/                 React tutorial in Hindi #35 Nested List in ReactJs
+// </===============->/
+
 import "./App.css";
 import React from "react";
 import { Table } from "react-bootstrap";
@@ -539,6 +581,7 @@ function App() {
       <Table striped variant="dark">
         <tbody>
           <tr>
+            <td>S.No</td>
             <td>Name</td>
             <td>Email</td>
             <td>Contacts</td>
@@ -546,6 +589,7 @@ function App() {
           {users.map((item, i) =>
             item.contact === "111" ? (
               <tr key={i}>
+                <td>{i}</td>
                 <td>{item.name}</td>
                 <td>{item.email}</td>
                 <td>{item.contact}</td>

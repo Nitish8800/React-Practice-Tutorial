648358624ceb807240fd789e9e63bb0e0962e9b6
Nitish8800 <nitishfea120784@gmail.com>
2022-06-10 00:57:42 +0530

    React tutorial in Hindi #25 componentDidUpdate life cycle method in ReactJs

--- react_tutorial_3_state-fun/src/App.js
+++ react_tutorial_3_state-fun/src/App.js
@@ -276,7 +276,37 @@
 
 // export default App;
 
-// <------------------------------>/                 React tutorial in Hindi #24 componentDidMount life cycle method in ReactJs
+// // <------------------------------>/                 React tutorial in Hindi #24 componentDidMount life cycle method in ReactJs
+// // </===============->/
+
+// import logo from "./logo.svg";
+// import "./App.css";
+// import React from "react";
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     console.warn("constructor");
+//     this.state = { name: "Nitish" };
+//   }
+//   componentDidMount() {
+//     console.warn("componentDidMount");
+//   }
+//   render() {
+//     console.warn("render");
+
+//     return (
+//       <div className="App">
+//         <h1>Component Did Mount {this.state.name}</h1>
+//         <button onClick={() => this.setState({ name: "NiKU" })}>Update</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+//
+
+// <------------------------------>/                 React tutorial in Hindi #25 componentDidUpdate life cycle method in ReactJs
 // </===============->/
 
 import logo from "./logo.svg";
@@ -286,18 +316,20 @@ class App extends React.Component {
   constructor() {
     super();
     console.warn("constructor");
-    this.state = { name: "Nitish" };
+    this.state = { count: 0 };
   }
-  componentDidMount() {
-    console.warn("componentDidMount");
+  componentDidUpdate(preProps, preState, snapshot) {
+    console.warn("componentDidUpdate", preState);
   }
   render() {
     console.warn("render");
 
     return (
       <div className="App">
-        <h1>Component Did Mount {this.state.name}</h1>
-        <button onClick={() => this.setState({ name: "NiKU" })}>Update</button>
+        <h1>Component Did Mount {this.state.count}</h1>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Update
+        </button>
       </div>
     );
   }

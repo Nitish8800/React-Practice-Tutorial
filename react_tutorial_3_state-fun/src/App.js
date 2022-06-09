1af3fea0a2e3c5e4dee6b5bc7685a69b24a5dbe7
Nitish8800 <nitishfea120784@gmail.com>
2022-06-10 02:31:26 +0530

    React tutorial in Hindi #34 List with Bootstrap Table

--- react_tutorial_3_state-fun/src/App.js
+++ react_tutorial_3_state-fun/src/App.js
@@ -492,33 +492,33 @@
 // // <------------------------------>/                 React tutorial in Hindi #33 Bootstrap in ReactJs
 // </===============->/
 
-// import './App.css';
-// import React from 'react'
-// import {Button,Badge,Card} from 'react-bootstrap'
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Install Bootstrap</h1>
-//       <h1>
-//     Example heading <Badge variant="light">New</Badge>
-//   </h1>
+import "./App.css";
+import React from "react";
+import { Button, Badge, Card } from "react-bootstrap";
+function App() {
+  return (
+    <div className="App">
+      <h1>Install Bootstrap</h1>
+      <h1>
+        Example heading <Badge variant="light">New</Badge>
+      </h1>
 
-//   <Card style={{ width: '18rem' }}>
-//   <Card.Img variant="top" src="holder.js/100px180" />
-//   <Card.Body>
-//     <Card.Title>Card Title</Card.Title>
-//     <Card.Text>
-//       Some quick example text to build on the card title and make up the bulk of
-//       the card's content.
-//     </Card.Text>
-//     <Button variant="primary">Go somewhere</Button>
-//   </Card.Body>
-// </Card>
-//     </div>
-//   );
-// }
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
 // // <------------------------------>/                 React tutorial in Hindi #34 List with Bootstrap Table
 // </===============->/

6af2b0052b07b99f9f995270c6eb923e09554d5a
Nitish8800 <nitishfea120784@gmail.com>
2022-06-10 01:30:25 +0530

    React tutorial in Hindi #28 Hooks in ReactJs

--- react_tutorial_3_state-fun/src/App.js
+++ react_tutorial_3_state-fun/src/App.js
@@ -404,7 +404,9 @@
 import "./App.css";
 import React, { useState } from "react";
 function App() {
-  const [data, setData] = useState("Nitish ");
+  const [data, setData] = useState("Nitish ")
+
+  
   return (
     <div className="App">
       <h1>Hooks in React {data}</h1>

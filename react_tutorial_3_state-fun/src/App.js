dc937c8a6debf391758e5b65dd9ca00a70257272
Nitish8800 <nitishfea120784@gmail.com>
2022-06-10 00:45:36 +0530

    \React tutorial in Hindi #24 componentDidMount life cycle method in ReactJs

--- react_tutorial_3_state-fun/src/App.js
+++ react_tutorial_3_state-fun/src/App.js
@@ -296,7 +296,6 @@ class App extends React.Component {
 
     return (
       <div className="App">
-      
         <h1>Component Did Mount {this.state.name}</h1>
        
         <button onClick={() => this.setState({ name: "NiKU" })}>Update</button>

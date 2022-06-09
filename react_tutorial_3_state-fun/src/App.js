c15d1d14bbb4f81bc71f17473187198a17f0965c
Nitish8800 <nitishfea120784@gmail.com>
2022-06-10 02:26:09 +0530

    React tutorial in Hindi #34 List with Bootstrap Table

--- react_tutorial_3_state-fun/src/App.js
+++ react_tutorial_3_state-fun/src/App.js
@@ -488,31 +488,76 @@
 // }
 
 // export default App;
+
+
+// // <------------------------------>/                 React tutorial in Hindi #33 Bootstrap in ReactJs
+// </===============->/
+
+// import './App.css';
+// import React from 'react'
+// import {Button,Badge,Card} from 'react-bootstrap'
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Install Bootstrap</h1>
+//       <h1>
+//     Example heading <Badge variant="light">New</Badge>
+//   </h1>
+
+//   <Card style={{ width: '18rem' }}>
+//   <Card.Img variant="top" src="holder.js/100px180" />
+//   <Card.Body>
+//     <Card.Title>Card Title</Card.Title>
+//     <Card.Text>
+//       Some quick example text to build on the card title and make up the bulk of
+//       the card's content.
+//     </Card.Text>
+//     <Button variant="primary">Go somewhere</Button>
+//   </Card.Body>
+// </Card>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
 // // <------------------------------>/                 React tutorial in Hindi #33 Bootstrap in ReactJs
 // </===============->/
 
 import './App.css';
 import React from 'react'
-import {Button,Badge,Card} from 'react-bootstrap'
+import {Table} from 'react-bootstrap'
 function App() {
+  const users = [
+    { name: 'Anil', email: 'anil@test.com', contact: '111' },
+    { name: 'Burce', email: 'bruce@test.com', contact: '222' },
+    { name: 'Peter', email: 'peter@test.com', contact: '111' },
+    { name: 'Sam', email: 'sam@test.com', contact: '777' },
+  ]
   return (
     <div className="App">
-      <h1>Install Bootstrap</h1>
-      <h1>
-    Example heading <Badge variant="light">New</Badge>
-  </h1>
-
-  <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+      <h1>List With Bootstrap Table</h1>
+      <Table striped variant="dark" >
+        <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Contacts</td>
+        </tr>
+        {
+          users.map((item,i)=>
+          item.contact==='111'?
+          <tr key={i}>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+        <td>{item.contact}</td>
+        </tr>:null
+          )
+        }
+        </tbody>
+      </Table>
     </div>
   );
 }
